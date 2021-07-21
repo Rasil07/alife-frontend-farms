638874dce952b6897fbd1999d0a893eae2cfbcb1
shrijana <shrijana1991@gmail.com>
2021-07-21 20:54:23 +0545

    increase total supply distribution

--- src/views/NewNft/components/NftProgressSimple.tsx
+++ src/views/NewNft/components/NftProgressSimple.tsx
@@ -14,7 +14,7 @@ const NftProgressSimple = () => {
         <InfoRow>
           <Text>{TranslateString(999, 'Total Pink Shibari Collection Claimed')}:</Text>
           <Text>
-            <strong>{!isInitialized ? '...' : `${currentDistributedSupply}/5`}</strong>
+            <strong>{!isInitialized ? '...' : `${currentDistributedSupply}/9`}</strong>
           </Text>
         </InfoRow>
       </CardBody>
